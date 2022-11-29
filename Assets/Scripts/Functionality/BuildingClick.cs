33ebc8798dca36b4dbf7d93edeaffc1d29d1ff2e
Mark Joseph Batinga <91181348+MarkJosephBatinga@users.noreply.github.com>
2022-11-29 18:58:23 +0800

    Save #d Map
    Save 3d Map

--- Assets/Scripts/Functionality/BuildingClick.cs
+++ Assets/Scripts/Functionality/BuildingClick.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BuildingClick : MonoBehaviour
@@ -7,11 +8,20 @@ public class BuildingClick : MonoBehaviour
     Ray ray;
     RaycastHit hit;
 
+    [SerializeField]
+    GameObject Build3dPrefab;
+
+    [SerializeField]
+    Material DefaultColor;
+
+    [SerializeField]
+    Material ColorChange;
+
     void Update()
     {
 
         ///// PICK UP IF USER TAPS
-        if (Input.touchCount > 0 &&  Input.GetTouch(0).phase == TouchPhase.Stationary)
+        if (Input.touchCount > 0 &&  Input.GetTouch(0).phase == TouchPhase.Began)
     {
             ////// CAST RAY TO PICK UP HITS
             ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
@@ -20,7 +30,22 @@ public class BuildingClick : MonoBehaviour
             {
                 if (hit.transform.position == transform.position)
                 {
-                    Debug.Log("building");
+                    if(Build3dPrefab != null)
+                    {
+                        var Last3dPrefab = GameObject.FindGameObjectWithTag("Build3dPrefab");
+                        if(Last3dPrefab != null)
+                        {
+                            var buildingName = Last3dPrefab.transform.Find("BuildName").GetComponent<TextMeshPro>().text;
+                            GameObject.Find(buildingName).GetComponent<MeshRenderer>().material = DefaultColor;
+                            GameObject.Destroy(Last3dPrefab);
+                        }
+                        var IBuildPrefab = GameObject.Instantiate(Build3dPrefab,
+                        new Vector3(transform.position.x, GetComponent<BoxCollider>().size.z * 9,
+                        transform.position.z), Quaternion.identity);
+
+                        IBuildPrefab.transform.Find("BuildName").GetComponent<TextMeshPro>().text = this.name;
+                        transform.GetComponent<MeshRenderer>().material = ColorChange;
+                    }
                 }
             } //// END OF RAYCAST
         }  //// END OF TOUCH BEGAN
